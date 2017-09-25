7a52ed0e497468a852a0c3eb5ca21d25c585e38e
billelc1 charef <billelc1@gmail.com>
2017-09-25 16:52:34 +0100

    Completed the Interface theme and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,7 +1,6 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,8 +11,25 @@ public class PageController {
 	public ModelAndView index()
 	{
 	ModelAndView mv = new ModelAndView("page");
-	mv.addObject("greeting","Welcome to Spring Web Mvc ");
+	mv.addObject("title","Home");
+	mv.addObject("userClickHome",true);
 	return mv;
+		
+	}
+	@RequestMapping(value= "/about")
+	public ModelAndView about(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
+		return mv;
+		
+	}
+	@RequestMapping(value= "/contact")
+	public ModelAndView contact(){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;
 		
 	}
 
